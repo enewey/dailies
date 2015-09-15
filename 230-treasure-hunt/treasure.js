415bb2a0e9e3e265967e5f2332d2f8d5eb41e0af
enewey <narcodis@gmail.com>
2015-09-14 19:13:34 -0600

    fixed indentation, purely cosmetic

--- 230-treasure-hunt/treasure.js
+++ 230-treasure-hunt/treasure.js
@@ -1,16 +1,16 @@
-	var fs = require('fs');
-	fs.readFile(process.argv[2], {encoding: 'utf-8'}, function(err, json) {
-		hunt(JSON.parse(json), "");
-	});
+var fs = require('fs');
+fs.readFile(process.argv[2], {encoding: 'utf-8'}, function(err, json) {
+	hunt(JSON.parse(json), "");
+});
 
-	function hunt(data, path) {
-		for (var prop in data) {
-			if (typeof data[prop] === "object" || typeof data[prop] === "array"){
-				if (hunt(data[prop], path + prop + " -> "))	
-					return true;
-			} else if (data[prop] == "dailyprogrammer") {
-				console.log(path + prop);
+function hunt(data, path) {
+	for (var prop in data) {
+		if (typeof data[prop] === "object" || typeof data[prop] === "array"){
+			if (hunt(data[prop], path + prop + " -> "))	
 				return true;
-			}
+		} else if (data[prop] == "dailyprogrammer") {
+			console.log(path + prop);
+			return true;
 		}
-	};
+	}
+};
--- 231-set-game-solver/setSolver.js
+++ 231-set-game-solver/setSolver.js
@@ -1,40 +1,40 @@
-	function makeCard(input, idNo) { //assuming length 4
-		return {
-			id : idNo, //unique identifier for this card
-			shape : input.charAt(0),
-			color : input.charAt(1),
-			number : input.charAt(2),
-			shading : input.charAt(3),
-			str : function() { return this.shape + this.color + this.number + this.shading; }
-		};
-	}
+function makeCard(input, idNo) { //assuming length 4
+	return {
+		id : idNo, //unique identifier for this card
+		shape : input.charAt(0),
+		color : input.charAt(1),
+		number : input.charAt(2),
+		shading : input.charAt(3),
+		str : function() { return this.shape + this.color + this.number + this.shading; }
+	};
+}
 
-	function determineMatch(cardA, cardB, cardC) {
-		for (var k in cardA)
-			if (!((cardA[k] == cardB[k] && cardB[k] == cardC[k]) || 
-				  (cardA[k] != cardB[k] && cardC[k] != cardB[k] && cardC[k] != cardA[k])))
-				return false;
-		return true;
-	}
+function determineMatch(cardA, cardB, cardC) {
+	for (var k in cardA)
+		if (!((cardA[k] == cardB[k] && cardB[k] == cardC[k]) || 
+			  (cardA[k] != cardB[k] && cardC[k] != cardB[k] && cardC[k] != cardA[k])))
+			return false;
+	return true;
+}
 
-	function findSets() {
-		var cards = [];
-		var sets = [];
+function findSets() {
+	var cards = [];
+	var sets = [];
 
-		for (var i=2; i<process.argv.length; i++)
-			cards.push(makeCard(process.argv[i], i-2));
+	for (var i=2; i<process.argv.length; i++)
+		cards.push(makeCard(process.argv[i], i-2));
 
-		for (var i=0; i<cards.length-2; i++) {
-			for (var j=i+1; j<cards.length-1; j++) {
-				for (var k=j+1; k<cards.length; k++) {
-					var key = [ cards[i].id, cards[j].id, cards[k].id ].sort().join();
-					if (determineMatch(cards[i], cards[j], cards[k]) && sets.indexOf(key) < 0) {
-						sets.push(key); //store the set as the id's separated by commas (id,id,id)
-						console.log(cards[i].str() + " " + cards[j].str() + " " + cards[k].str());
-					}
-						
+	for (var i=0; i<cards.length-2; i++) {
+		for (var j=i+1; j<cards.length-1; j++) {
+			for (var k=j+1; k<cards.length; k++) {
+				var key = [ cards[i].id, cards[j].id, cards[k].id ].sort().join();
+				if (determineMatch(cards[i], cards[j], cards[k]) && sets.indexOf(key) < 0) {
+					sets.push(key); //store the set as the id's separated by commas (id,id,id)
+					console.log(cards[i].str() + " " + cards[j].str() + " " + cards[k].str());
 				}
+					
 			}
-		}	
-	}
-	findSets();
+		}
+	}	
+}
+findSets();
