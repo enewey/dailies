e190666b6fef1a0e2709acf10bab3d8ba7bc4056
enewey <narcodis@gmail.com>
2015-09-15 18:55:10 -0600

    fixed indentation on some files

--- 222Easy-balancing-words/wordBalancer.js
+++ 222Easy-balancing-words/wordBalancer.js
@@ -1,10 +1,10 @@
-	function wordBalancer(word) {
-		for (var i=0; i<word.length; i++) {
-			var left=0, right=0; 
-			for (var k=1; k<=i; k++) left += (word.charAt(i-k).charCodeAt(0) - 64)*k;
-			for (var k=1; i+k<word.length; k++) right += (word.charAt(i+k).charCodeAt(0) - 64)*k;
-			if (left==right) 
-				return word.slice(0,i) + " " + word.charAt(i) + " " + word.slice(i+1, word.length) + " - " + left;
-		}
-		return word + " DOES NOT BALANCE";
+function wordBalancer(word) {
+	for (var i=0; i<word.length; i++) {
+		var left=0, right=0; 
+		for (var k=1; k<=i; k++) left += (word.charAt(i-k).charCodeAt(0) - 64)*k;
+		for (var k=1; i+k<word.length; k++) right += (word.charAt(i+k).charCodeAt(0) - 64)*k;
+		if (left==right) 
+			return word.slice(0,i) + " " + word.charAt(i) + " " + word.slice(i+1, word.length) + " - " + left;
 	}
+	return word + " DOES NOT BALANCE";
+}
